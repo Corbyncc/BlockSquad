6210253f3e5adf2c517d5ac3966c2f4eec70bbfd
Corbyn <corbyn@hyperplugins.et>
2024-10-19 14:02:00 -0500

    Add ARG for GitHubToken to pass along to image on build

--- BlockSquad.Server/pull-game.sh
+++ BlockSquad.Server/pull-game.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-# Get the latest successful workflow run
 RUN_ID=$(curl -s -H "Authorization: token $GitHubToken" \
   "https://api.github.com/repos/corbyncc/blocksquad/actions/workflows/build-game.yml/runs?status=success&per_page=1" |
   jq -r '.workflow_runs[0].id')
 
-# Get the artifact ID
 ARTIFACT_ID=$(curl -s -H "Authorization: token $GitHubToken" \
   "https://api.github.com/repos/corbyncc/blocksquad/actions/runs/$RUN_ID/artifacts" |
   jq -r '.artifacts[0].id')
 
-# Download the artifact
 curl -L -H "Authorization: token $GitHubToken" \
   -o artifact.zip \
   "https://api.github.com/repos/corbyncc/blocksquad/actions/artifacts/$ARTIFACT_ID/zip"
